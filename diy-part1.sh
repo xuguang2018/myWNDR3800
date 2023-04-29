7d6c41d830f68bdd4d889a35a583c4823bc80d2a
xuguang2018 <69624955+xuguang2018@users.noreply.github.com>
2023-04-29 16:51:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,9 +19,9 @@
 ./scripts/feeds install -a
 
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git smartdns https://github.com/pymumu/smartdns.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #
 ./scripts/feeds update -a && ./scripts/feeds install -a
 #
